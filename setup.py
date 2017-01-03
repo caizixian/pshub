5065a609c682bcaa617a058110274b3e2b04ec18
Ivan Cai <caizixian@users.noreply.github.com>
2017-01-03 17:17:37 +0800

    Bump version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 setup(
     name='pshub',
 
-    version='0.0.1',
+    version='0.0.2',
 
     description='A Pub/Sub framework implemented in Python',
     long_description=long_description,
